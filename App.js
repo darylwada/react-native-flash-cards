28a73c9e3433cda39920f40c4a0ce5809b398c59
darylwada <daryl.wada@gmail.com>
2018-08-09 23:07:52 -0700

    Refactored to use createMaterialTopTabNavigator. #3

--- App.js
+++ App.js
@@ -1,45 +1,24 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
 import Form from './views/Form'
-import Nav from './components/Nav'
 import List from './views/List'
-import { createStackNavigator } from 'react-navigation'
+import { createMaterialTopTabNavigator } from 'react-navigation';
 
-const RootStack = createStackNavigator(
+
+
+export default createMaterialTopTabNavigator(
   {
-    new: Form,
-    list: List,
+    'New Card': { screen: Form },
+    Cards: { screen: List },
   },
   {
-    initialRouteName: 'new',
-  }
-);
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      view: 'new',
-      savedCards: []
+    tabBarOptions: {
+      activeTintColor: 'rgb(108, 209, 165)',
+      inactiveTintColor: 'gray',
+      style: {
+          backgroundColor: '#fff',
+      },
+      indicatorStyle: {
+          backgroundColor: 'rgb(108, 209, 165)',
+      },
     }
-    this.handleSave = this.handleSave.bind(this)
   }
-
-  handleSave(newCard) {
-    const savedCards = [...this.state.savedCards, newCard]
-    this.setState({ savedCards })
-  }
-
-  render() {
-    console.log(this.state)
-    return <RootStack />
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'rgb(233, 236, 239)',
-    alignItems: 'center',
-  }
-})
+)
--- views/Form.js
+++ views/Form.js
@@ -18,39 +18,42 @@ export default class Form extends React.Component {
   }
 
   render() {
-    // const display = this.props.view === 'new'
-    //   ? styles.container
-    //   : styles.hidden
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>Create a Flash Card</Text>
-        <Text style={styles.label}>Question</Text>
-        <TextInput 
-          style={{height: 30, width: 250, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={question => this.setState({ question })}
-          value={this.state.question}
-        />
-        <Text style={styles.label}>Answer</Text>
-        <TextInput 
-          style={{height: 30, width: 250, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={answer => this.setState({ answer })}
-          value={this.state.answer}
-        />
-        <View style={styles.button}>
-          <Button
-            onPress={this.handleSave}
-            title="Save"
-            color="white"
+        <View style={styles.card}>
+          <Text style={styles.header}>Create a Flash Card</Text>
+          <Text style={styles.label}>Question</Text>
+          <TextInput 
+            style={{height: 30, width: 250, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={question => this.setState({ question })}
+            value={this.state.question}
           />
+          <Text style={styles.label}>Answer</Text>
+          <TextInput 
+            style={{height: 30, width: 250, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={answer => this.setState({ answer })}
+            value={this.state.answer}
+          />
+          <View style={styles.button}>
+            <Button
+              onPress={this.handleSave}
+              title="Save"
+              color="white"
+            />
+          </View>
         </View>
       </View>
     )
   }
-
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: 'rgb(233, 236, 239)',
+    alignItems: 'center',
+  },
+  card: {
     backgroundColor: 'white',
     width: 300,
     height: 250,
@@ -61,9 +64,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  // hidden: {
-  //   display: none
-  // },
   header: {
     marginBottom: 25
   },
